2e4faedef7b85f438c4f2e978408ce18f1743591
Seyn <hub6dom99@gmail.com>
2021-06-23 13:30:24 +0200

    Add project files.

---
+++ WeatherService/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherService.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        DbSet<WeatherInfo> WeatherInfos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+
+    public class WeatherInfo
+    {
+        public int Id { get; set; }
+        public string WeatherServiceName { get; set; }
+        public string City { get; set; }
+        public DateTime DateTime { get; set; }
+        public double Temperature { get; set; }
+        public double Pressure { get; set; }
+        public double Humidity { get; set; }
+        public double Rain { get; set; }
+        public int WindSpeed { get; set; }
+        public int WindDirection { get; set; }
+    }
+}
---
+++ WeatherService/Data/Migrations/20210623112807_AddWeatherInfoTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeatherService.Data.Migrations
+{
+    public partial class AddWeatherInfoTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WeatherServiceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Temperature = table.Column<double>(type: "float", nullable: false),
+                    Pressure = table.Column<double>(type: "float", nullable: false),
+                    Humidity = table.Column<double>(type: "float", nullable: false),
+                    Rain = table.Column<double>(type: "float", nullable: false),
+                    WindSpeed = table.Column<int>(type: "int", nullable: false),
+                    WindDirection = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherInfos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherInfos");
+        }
+    }
+}
